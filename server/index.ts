9416ae096e67d917f667f86003ef7729825a7e72
coelho <fabianocoelho8844@gmail.com>
2022-04-23 17:44:30 -0300

    armazenando id da sala em uma variavel para evitar de emitir o evento para todas as salas existentes

--- server/index.ts
+++ server/index.ts
@@ -25,29 +25,25 @@ const ioServer = new io.Server(server, {
 });
 
 ioServer.on('connection', (socket) => {
-    
+    let _roomId = '';
     socket.on('join', function({roomId}) {
         if(roomId.length > 5) {
             return socket.emit('error', 'Invalid room')
         }
+        _roomId = roomId;
         socket.join(roomId);
     });
 
     socket.on('sendValue', (value) => {
-        const rooms = Object.keys(Object.fromEntries(ioServer.sockets.adapter.rooms)).filter(room => room.length <= 5);
-        ioServer.to(rooms).emit('receiveValue', {value, id: socket.id})
+        ioServer.to(_roomId).emit('receiveValue', {value, id: socket.id})
     })
 
     socket.on('slot-animate', function(startAnimation: boolean) {
-        const rooms = Object.keys(Object.fromEntries(ioServer.sockets.adapter.rooms)).filter(room => room.length <= 5);
-        ioServer.to(rooms).emit('slot-animate', startAnimation);
+        ioServer.to(_roomId).emit('slot-animate', startAnimation);
     });
 
     socket.on('disconnect', () => {
-        const rooms = Object.keys(Object.fromEntries(ioServer.sockets.adapter.rooms)).filter(room => room.length <= 5);
-        rooms.forEach(roomId => {
-            ioServer.to(roomId).emit('disconnected', socket.id)
-        })
+        ioServer.to(_roomId).emit('disconnected', socket.id)
     });
 });
   

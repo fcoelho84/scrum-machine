0cd672698793df9b75a9dae2b776f4db77e48aa2
Coelho <fabianocoelho8844@gmail.com>
2024-10-06 11:13:31 -0300

    fix eslint and prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+  tabWidth: 2,
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
 
-export default config;
+export default config

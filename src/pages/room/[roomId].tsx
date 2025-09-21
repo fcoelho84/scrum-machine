0329c70e9459c53cce35de53782021095487f61f
coelho <fabianocoeolho8844@gmail.com>
2025-09-21 16:16:04 -0300

    redirected anonymous users to the home screen to take a name

--- src/pages/room/[roomId].tsx
+++ src/pages/room/[roomId].tsx
@@ -1,43 +1,39 @@
-import { useParams } from 'next/navigation'
-import { type Room } from 'party/types'
-import { useMemo } from 'react'
-
-import { useSocketMessage } from '~/hooks/useSocket'
+import { useParams, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 import SlotMachine from '~/components/SlotMachine'
-import { mostRepeatedNumber } from '~/utils/numbers'
 import BackgroundPage from '~/components/BackgroundPage'
 import Options from '~/components/Options'
 import Controls from '~/components/Controls'
 
 const Room = () => {
   const params = useParams()
-  const users = useSocketMessage<Room['users']>((state) => state?.users)
+  const router = useRouter()
 
-  const nonSpectators = useMemo(
-    () => users?.filter((user) => user.state !== 'spectator') ?? [],
-    [users]
-  )
-
-  const repeatedNumber = useMemo(() => {
-    if (nonSpectators.find((user) => user.state === 'idle')) return '🤔'
-
-    return mostRepeatedNumber(nonSpectators.map((user) => user.point))
-  }, [nonSpectators])
+  useEffect(() => {
+    const name = sessionStorage.getItem('name')
+    if (!name && params?.roomId) {
+      const roomId = Array.isArray(params.roomId)
+        ? params.roomId[0]
+        : params.roomId
+      router.push(`/?roomId=${roomId}`)
+    }
+  }, [params?.roomId, router])
 
   if (!params?.roomId) {
+    return <></>
+  }
+
+  const name = sessionStorage.getItem('name')
+
+  if (!name) {
     return <></>
   }
 
   return (
     <BackgroundPage>
       <div className="relative z-10 flex min-h-screen w-full flex-col items-center justify-center gap-4 p-4 sm:gap-6 sm:p-6">
-        <span className="text-center text-4xl sm:text-6xl">
-          {repeatedNumber ?? '🤔'}
-        </span>
-
         <SlotMachine />
-
         <Options />
         <Controls />
       </div>
